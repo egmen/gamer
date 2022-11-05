ff63db985087d91865eda3e25aabe40fefbde13c
egmen <egmen@mail.ru>
2022-11-05 21:29:30 +0300

    $@

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,18 @@ const rootElement = document.getElementById("root");
 if (!rootElement) {
   throw new Error(`Не найден элемент #root`);
 }
+
+async function wakeLock() {
+  if ("wakeLock" in navigator) {
+    // @ts-ignore
+    await navigator.wakeLock.request("screen");
+  }
+}
+
+wakeLock();
+
 const root = ReactDOM.createRoot(rootElement);
+
 root.render(
   <App>
     <Timer />
